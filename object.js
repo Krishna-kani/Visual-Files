68ac913a68b224fb7b52e9144008bb4e2a0bc186
Krishna-kani <91647632+Krishna-kani@users.noreply.github.com>
2021-09-30 21:25:00 -0700

    Second file

---
+++ object.js
@@ -0,0 +1,18 @@
+var car1 = {
+    "name": "honda",
+    "model": "focus",
+    "color": "black",
+    "wheels": 4
+};
+var car2 = {
+    "name": "honda",
+    "model": "direct",
+    "color": "white",
+    "wheels": 2
+};
+if ("honda"== "honda")
+{
+    console.log("same");
+} else {
+console.log("different");
+}
